7c118ee519c45eb7bf0920649b389b3e249d102f
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-01-14 20:55:34 +0100

    Add navbar component and update dependencies

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,19 @@
+
+function Navbar() {
+  return (
+    <div>
+    <nav className="color">
+      <ul className="color">
+        <li>
+          <h1>Navbar</h1>
+        </li>
+        <li>
+          <h2>Navbar</h2>
+        </li>
+      </ul>
+    </nav>
+    </div>
+  );
+}
+
+export default Navbar;
