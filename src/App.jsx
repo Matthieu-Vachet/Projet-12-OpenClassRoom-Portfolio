b863e33d142726b90a401991f57f37e64b25e39e
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-01-16 15:28:57 +0100

    Add loader component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,29 @@ import Home from './pages/home';
 import NavBar from './components/navbar';
 
 import '../src/style/global.scss';
+import Loader from './components/loader';
+import { useEffect, useState } from 'react';
 
 function App() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <>
-            <NavBar />
-            <Home />
+            {loading ? (
+                <Loader />
+            ) : (
+                <>
+                    <NavBar />
+                    <Home />
+                </>
+            )}
         </>
     );
 }
