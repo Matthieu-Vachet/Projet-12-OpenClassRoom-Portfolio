6ad86f214c3dec04f5b2058199c2b061a9a08153
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-01-30 20:55:43 +0100

    Update styles and toggle functionality

--- src/components/lightDarkToggle/index.jsx
+++ src/components/lightDarkToggle/index.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
-import { CiLight } from "react-icons/ci";
-import { MdDarkMode } from "react-icons/md";
+import { useState, useEffect } from 'react';
+import { CiLight } from 'react-icons/ci';
+import { MdDarkMode } from 'react-icons/md';
 
 import './style.scss';
 
@@ -11,38 +11,49 @@ import './style.scss';
  */
 
 const LightDarkToggle = () => {
-  // State to track the current mode (light or dark)
-  const [islightMode, setLightMode] = useState(false);
+    // State to track the current mode (light or dark)
+    const [islightMode, setLightMode] = useState(false);
 
-  // Function to toggle between light and dark mode
-  const toggleMode = () => {
-    setLightMode(!islightMode);
-  };
+    // Function to toggle between light and dark mode
+    const toggleMode = () => {
+        setLightMode(!islightMode);
+    };
 
-  // Apply the selected mode's colors using CSS custom properties
-  useEffect(() => {
-    const colors = !islightMode
-      ? {
-          // Light Colors
-          "--basicBlack": "rgba(13, 13, 13, 1)",
-          
+    // Apply the selected mode's colors using CSS custom properties
+    useEffect(() => {
+        const colors = !islightMode
+            ? {
+                  // Black Colors
+                  '--basicBlack': 'rgba(13, 13, 13, 1)',
+                  '--darkWhite70': 'rgba(255, 255, 255, 0.7)',
+                  '--topBackgroundBlue': 'rgba(63, 100, 233, 0.55)',
+                  '--topBackgroundRed': 'rgba(233, 63, 63, 0.55)',
+                  '--topBackgroundYellow': 'rgba(255, 184, 0, 0.55)',
+              }
+            : {
+                  // Light Colors
+                  '--basicBlack': 'rgba(255, 255, 255, 1)',
+                  '--darkWhite70': 'rgba(13, 13, 13, 0.7)',
+                  '--topBackgroundBlue': 'rgba(63, 100, 233, 1)',
+                  '--topBackgroundRed': 'rgba(233, 63, 63, 1)',
+                  '--topBackgroundYellow': 'rgba(255, 184, 0, 1)',
+              };
+
+        for (const property in colors) {
+            const value = colors[property];
+            document.documentElement.style.setProperty(property, value);
         }
-      : {
-          // Dark Colors
-          "--basicBlack": "rgba(255, 255, 255, 1)",
-        };
+    }, [islightMode]);
 
-    for (const property in colors) {
-      const value = colors[property];
-      document.documentElement.style.setProperty(property, value);
-    }
-  }, [islightMode]);
-
-  return (
-    <button className="toggleMode" onClick={toggleMode}>
-      {islightMode ? <CiLight className="toggleIcon" /> : <MdDarkMode className="toggleIcon" />}
-    </button>
-  );
+    return (
+        <button className='toggleMode' onClick={toggleMode}>
+            {islightMode ? (
+                <CiLight className='toggleIcon' />
+            ) : (
+                <MdDarkMode className='toggleIcon' />
+            )}
+        </button>
+    );
 };
 
 export default LightDarkToggle;
