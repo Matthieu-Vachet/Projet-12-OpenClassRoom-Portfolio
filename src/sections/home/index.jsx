dc631c1f52a8d84cbf43b0e3a4e4e4217dd51ad2
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-02-06 16:46:45 +0100

    Ajouter un commentaire pour LineBackground dans la page d'accueil

--- src/sections/home/index.jsx
+++ src/sections/home/index.jsx
@@ -2,12 +2,14 @@ import TopBackground from '../../components/topBackground';
 import Header from '../../layouts/header';
 import Hero from '../../layouts/hero';
 import SocialBar from '../../components/socialBar';
+// import LineBackground from '../../components/background/lineBackground';
 
 import './style.scss';
 
 export default function Home() {
     return (
         <section id='home'>
+            {/* <LineBackground /> */}
             <Header />
             <TopBackground />
             <div className='heroBanner'>
