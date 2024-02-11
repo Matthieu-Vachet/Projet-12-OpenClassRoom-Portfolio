b34d658075d1c0aea755901b15a600fc9e5e0078
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-02-11 15:46:50 +0100

    Add UserContext, ProjetContext, ExperienceContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { DataProvider } from './utils/dataProvider/DataProvider.jsx';
 import App from './App.jsx';
 
 import '../src/styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <DataProvider>
+            <App />
+        </DataProvider>
     </React.StrictMode>,
 );
