9ca2e6fc8750b3ee82bca75b0a6b55b75b738747
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-03-09 19:00:11 +0100

    Ajouter SpeedInsights et Analytics dans le rendu de l'application

--- src/main.jsx
+++ src/main.jsx
@@ -35,9 +35,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <I18nextProvider i18n={i18n}>
             <Suspense fallback='Loading...'>
                 <DataProvider>
-                    <App />
                     <SpeedInsights />
                     <Analytics />
+                    <App />
                 </DataProvider>
             </Suspense>
         </I18nextProvider>
