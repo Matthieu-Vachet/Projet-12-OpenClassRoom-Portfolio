05aec214a4760c24d09b94f6494a5c9121b93d9c
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-03-09 16:38:27 +0100

    Ajouter robots.txt et sitemap.xml, mettre à jour package.json et supprimer Structure.md

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 /**
  * @file App.jsx
  * @module App
- * @description Ce module est le composant principal de l'application. Il gère l'affichage de l'application et affiche un loader pendant le chargement.
+ * @description Ce module est le composant principal de l'application. Il gère l'affichage de l'application .
  * Une fois le chargement terminé, il affiche l'application.
  * @requires react
  * @requires framer-motion
  * @requires ./layouts/navbar
- * @requires ./layouts/loader
  * @requires ./sections/home
  * @requires ./sections/about
  * @requires ./components/background/overlay/blur
@@ -21,12 +20,11 @@
  */
 
 /* Importation des modules */
-import { useEffect, useState, lazy, Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 
 /* Importation des composants */
 import NavBar from './layouts/navbar';
-import Loader from './layouts/loader';
 import Home from './sections/home';
 const About = lazy(() => import('./sections/about'));
 const Blur = lazy(() => import('./components/background/overlay/blur'));
@@ -38,7 +36,7 @@ const Footer = lazy(() => import('./layouts/footer'));
 import { Toaster } from 'sonner';
 
 /* Importation des animations */
-import { LoaderVariants, AppVariants } from './utils/framerMotion/Variante';
+import { AppVariants } from './utils/framerMotion/Variante';
 
 /* Importation des assets */
 import backgroundImage from './assets/images/Bg-Squares-Black.svg';
@@ -47,69 +45,42 @@ import backgroundImage from './assets/images/Bg-Squares-Black.svg';
     * Composant App
     * Description :
         - Gère l'affichage de l'application
-        - Affiche un loader pendant le chargement
-        - Affiche l'application une fois le chargement terminé
     * @return {JSX.Element} Le composant App
 */
 
 function App() {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        setLoading(true);
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 2500);
-
-        return () => {
-            clearTimeout(timer);
-        };
-    }, []);
-
     return (
         <AnimatePresence>
-            {loading ? (
-                <motion.div
-                    key='loader'
-                    initial='initial'
-                    animate='animate'
-                    exit='exit'
-                    variants={LoaderVariants}
-                >
-                    <Loader />
-                </motion.div>
-            ) : (
-                <motion.div
-                    key='app'
-                    initial='initial'
-                    animate='animate'
-                    exit='exit'
-                    variants={AppVariants}
-                    style={{
-                        backgroundImage: `url(${backgroundImage})`,
+            <motion.div
+                key='app'
+                initial='initial'
+                animate='animate'
+                exit='exit'
+                variants={AppVariants}
+                style={{
+                    backgroundImage: `url(${backgroundImage})`,
+                }}
+            >
+                <Blur />
+                <NavBar />
+                <Home />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <About />
+                    <Experience />
+                    <Skills />
+                    <Projects />
+                    <Contact />
+                    <Footer />
+                </Suspense>
+                <Toaster
+                    richColors
+                    position='top-right'
+                    toastOptions={{
+                        closeButton: true,
+                        className: 'my-toast',
                     }}
-                >
-                    <Blur />
-                    <NavBar />
-                    <Home />
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <About />
-                        <Experience />
-                        <Skills />
-                        <Projects />
-                        <Contact />
-                        <Footer />
-                    </Suspense>
-                    <Toaster
-                        richColors
-                        position='top-right'
-                        toastOptions={{
-                            closeButton: true,
-                            className: 'my-toast',
-                        }}
-                    />
-                </motion.div>
-            )}
+                />
+            </motion.div>
         </AnimatePresence>
     );
 }
