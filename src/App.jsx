767705ec4adaa3b39bb39796f3efa8141023b03f
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-03-11 17:48:58 +0100

    Supprimer le composant Loader et mettre à jour la structure de l'application

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,10 @@
  */
 
 /* Importation des modules */
-import { lazy, Suspense, useEffect, useState } from 'react';
+import { lazy, Suspense } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 
 /* Importation des composants */
-import Loader from './layouts/loader';
 import NavBar from './layouts/navbar';
 import Home from './sections/home';
 import ParticlesCircle from './components/background/particles';
@@ -48,61 +47,36 @@ import { AppVariants } from './utils/framerMotion/Variante';
 */
 
 function App() {
-    // Etat pour gérer l'affichage du loader
-    const [loading, setLoading] = useState(true);
-
-    // Simule un temps de chargement de 5 secondes
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 1000);
-        return () => clearTimeout(timer);
-    }, []);
-
     return (
         <AnimatePresence>
-            {loading ? (
-                // Affiche le loader pendant le chargement
-                <motion.div
-                    key='loader'
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 0.5, ease: 'easeInOut' }}
-                >
-                    <Loader />
-                </motion.div>
-            ) : (
-                // Une fois le chargement terminé, affiche l'application
-                <motion.div
-                    key='app'
-                    initial='initial'
-                    animate='animate'
-                    exit='exit'
-                    variants={AppVariants}
-                >
-                    <ParticlesCircle />
-                    <Blur />
-                    <NavBar />
-                    <Home />
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <About />
-                        <Experience />
-                        <Skills />
-                        <Projects />
-                        <Contact />
-                        <Footer />
-                    </Suspense>
-                    <Toaster
-                        richColors
-                        position='top-right'
-                        toastOptions={{
-                            closeButton: true,
-                            className: 'my-toast',
-                        }}
-                    />
-                </motion.div>
-            )}
+            <ParticlesCircle />
+            <motion.div
+                key='app'
+                initial='initial'
+                animate='animate'
+                exit='exit'
+                variants={AppVariants}
+            >
+                <Blur />
+                <NavBar />
+                <Home />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <About />
+                    <Experience />
+                    <Skills />
+                    <Projects />
+                    <Contact />
+                    <Footer />
+                </Suspense>
+                <Toaster
+                    richColors
+                    position='top-right'
+                    toastOptions={{
+                        closeButton: true,
+                        className: 'my-toast',
+                    }}
+                />
+            </motion.div>
         </AnimatePresence>
     );
 }
